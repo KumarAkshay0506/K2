b5cf15eb9219fe7fcdf6c41f6c8c024f80ddb901
akshayk050695 <akshay.kumar2@mavs.uta.edu>
2018-06-18 00:03:50 -0500

    hii

--- Kmeans.py
+++ Kmeans.py
@@ -100,7 +100,7 @@ def main():
         pylab.scatter(cent[:,0],cent[:,1], marker='o', s = 400, linewidths=3, c='none')
         pylab.scatter(cent[:,0],cent[:,1], marker='x', s = 400, linewidths=3)
 
-        pylab.savefig("static/kmeans6.png")
+        pylab.savefig("static/kmeans4.png")
 
         return render_template('index.html',cdist=cdist,pdict=pdict, disCluster = disCluster)
 
